485a0e4d916fa6916b6b6b597963336a61baec75
Shane O'Mahony <sleepymole@users.noreply.github.com>
2019-01-11 20:52:32 +0000

    Add a button to the view and add a target/action handler to the button

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hitMeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        hitMeButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
     }
 
-
+    @objc func showAlert() {
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first message!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default) {
+                _ in
+                print("pressed the awesome alert button!")
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
