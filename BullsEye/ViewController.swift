115747c980939e78b99917b5951bea6fb003ece4
Shane O'Mahony <shane.omahony@workday.com>
2019-01-23 08:15:38 +0000

    added new elements to the storyboard, updated functionality to display value of slider in popup

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,19 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var hitMeButton: UIButton!
+    var currentValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         hitMeButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
+        currentValue = lroundf(slider.value)
     }
 
     @objc func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first message!", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Awesome", style: .default) {
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) {
                 _ in
                 print("pressed the awesome alert button!")
         }
@@ -32,5 +34,8 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 }
 
