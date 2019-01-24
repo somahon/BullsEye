e5be9b55a0c3d061804c2a1b978a9068f65f5de7
Shane O'Mahony <shane.omahony@workday.com>
2019-01-24 07:54:50 +0000

    added calculation for difference, updated alert message and added method to start new round

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,17 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var hitMeButton: UIButton!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     var currentValue: Int = 0
+    var targetValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         hitMeButton.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
-        currentValue = lroundf(slider.value)
+        startNewRound()
     }
 
     @objc func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let difference = calculateDifference()
+        let message = "The value of the slider is: \(currentValue)" +
+                        "\nThe target value is \(targetValue)" +
+                            "\nThe difference is: \(difference)"
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default) {
                 _ in
@@ -32,10 +38,36 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
+    
+    func calculateDifference() -> Int {
+        var difference: Int = 0
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
+        
+        return difference
     }
 }
 
