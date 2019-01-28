44ba61e33448bbfb7bb9ecabbea459f2cc1d4cbb
Shane O'Mahony <shane.omahony@workday.com>
2019-01-28 14:22:47 +0000

    added new view controller and screen for the info button

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Shane O'Mahony on 28/01/2019.
+//  Copyright © 2019 Shane O'Mahony. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
